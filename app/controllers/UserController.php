b3c873d26c0bd5b26cb30e2651ee54a8c40d6007
1603633284@qq.com <1603633284@qq.com>
2020-05-12 14:06:19 +0800

    测试

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -28,7 +28,7 @@ class UserController extends BaseController
      * */
 	public function mobileLoginAction()
 	{
-		echo 1111;
+		echo 1111;echo 22222;
 		//接收参数并格式化
 		$data = $this->request->getQuery();
 		$mobile = isset($data['mobile'])?substr(preg_replace('# #','',$data['mobile']),0,11):"";
