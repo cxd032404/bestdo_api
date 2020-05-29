cd652df78db80172dbfade44ef147b517c702d14
1603633284@qq.com <1603633284@qq.com>
2020-05-29 09:59:30 +0800

    用户登录成功后，自动调用完善用户微信资料方法

--- app/controllers/WechatController.php
+++ app/controllers/WechatController.php
@@ -10,18 +10,12 @@ use Phalcon\Mvc\Controller;
 class WechatController extends BaseController
 {
 
-    public function ceshiAction()
-    {
-        $user_id = 11879;
-        $getWechatUserAction = (new WechatController())->getWechatUserAction($user_id);
-        print_r($getWechatUserAction);die;
-    }
-
     /*更新用户微信信息*/
     public function getWechatUserAction($user_id=0)
     {
         $appid = $this->key_config->aliyun->wechat->appid;
         $appsecret = $this->key_config->aliyun->wechat->appsecret;
+        //判断是否在微信浏览器打开，不在微信浏览器打开无法获取code
         if($this->is_weixin()){
             if (empty($_REQUEST["code"])) {//第一步：获取微信授权code
                 $redirect_url = 'http://api.staffhome.cn/Wechat/getWechatUser';
@@ -32,39 +26,29 @@ class WechatController extends BaseController
                 $code = $_REQUEST['code']??"";
                 $user_id = $_REQUEST['state']??0;
                 $oauth2 = $this->getOauthAccessToken($appid,$appsecret,$code);
-                if (array_key_exists('errcode', $oauth2) && $oauth2['errcode'] != '0') {
-                    return $this->failure($oauth2,"网页授权access_token获取失败！");
-                }
-                $openid = $oauth2['openid'];
-                //第三步：根据网页授权access_token和openid获取用户信息（不包含是否关注）
-                $oauth_userinfo = $this->getOauthUserInfo($oauth2['access_token'],$openid);
-                if (array_key_exists('errcode', $oauth_userinfo) && $oauth_userinfo['errcode'] != '0') {
-                    return $this->failure($oauth_userinfo,"利用网页授权access_token获取用户信息失败！");
-                }
-                //修改用户信息
-                $userinfo = UserInfo::findFirst(["user_id = '".$user_id."' and is_del=0"]);
-                if($userinfo){
-                    $userinfo->wechatid = $oauth_userinfo['openid'];
-                    $userinfo->nick_name = $oauth_userinfo['nickname'];
-                    $userinfo->sex = $oauth_userinfo['sex'];
-                    $userinfo->user_img = $oauth_userinfo['headimgurl'];
-                    $userinfo->wechatinfo = json_encode($oauth_userinfo);
-                    if ($userinfo->update() === false) {
-                        return $this->failure([],"用户微信信息完善失败！");
-                    }else {
-                        return $this->success([],"用户微信信息完善成功！");
+                if (!array_key_exists('errcode', $oauth2)) {
+                    $openid = $oauth2['openid'];
+                    //第三步：根据网页授权access_token和openid获取用户信息（不包含是否关注）
+                    $oauth_userinfo = $this->getOauthUserInfo($oauth2['access_token'],$openid);
+                    if (!array_key_exists('errcode', $oauth_userinfo)) {
+                        //修改用户信息
+                        $userinfo = UserInfo::findFirst(["user_id = '".$user_id."' and is_del=0"]);
+                        if($userinfo){
+                            $userinfo->wechatid = $oauth_userinfo['openid'];
+                            $userinfo->nick_name = $oauth_userinfo['nickname'];
+                            $userinfo->sex = $oauth_userinfo['sex'];
+                            $userinfo->user_img = $oauth_userinfo['headimgurl'];
+                            $userinfo->wechatinfo = json_encode($oauth_userinfo);
+                            $userinfo->update();
+                        }
                     }
-                }else{
-                    return $this->failure([],"用户信息未查询到！");
                 }
             }
-        }else{
-            return $this->failure([],"当前页面不在微信浏览器，不可更新用户微信信息！");
         }
     }
 
 
-    /*获取用户信息并判断是否关注*/
+    /*测试----获取用户信息并判断是否关注*/
     public function indexAction()
     {
         $appid = $this->key_config->aliyun->wechat->appid;
@@ -103,8 +87,6 @@ class WechatController extends BaseController
         $return  = ['result'=>1, 'msg'=>"", 'code'=>200, 'data'=>['access_token_user_info'=>$userinfo, 'oauth_access_token_user_info'=>$oauth_userinfo]];
         return $this->success($return);
     }
-
-
 
     //查询是否在微信浏览器打开
     public function is_weixin(){
