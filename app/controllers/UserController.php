54743d5348e18b9adf7b565985ffd0a5ce4a370f
1603633284@qq.com <1603633284@qq.com>
2020-05-29 15:47:10 +0800

    注释登录获取授权code代码

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -63,13 +63,13 @@ class UserController extends BaseController
 		$code = isset($data['code'])?preg_replace('# #','',$data['code']):"";
 
 		//判断是否在微信浏览器打开，不在微信浏览器打开无法获取code
-		if((new WechatService)->is_weixin()) {
-			if (empty($code)) {//第一步：获取微信授权code
-				$redirect_url = "http://api.staffhome.cn/User/mobileCodeLogin?mobile=".$mobile."&logincode=".$logincode."companyuser_id=".$companyuser_id."";
-				(new WechatService)->getCode($this->key_config->aliyun->wechat->appid,$redirect_url,json_encode($data));
-				return;
-			}
-		}
+//		if((new WechatService)->is_weixin()) {
+//			if (empty($code)) {//第一步：获取微信授权code
+//				$redirect_url = "http://www.staffhome.cn/api/User/mobileCodeLogin?mobile=".$mobile."&logincode=".$logincode."companyuser_id=".$companyuser_id."";
+//				(new WechatService)->getCode($this->key_config->aliyun->wechat->appid,$redirect_url,json_encode($data));
+//				return;
+//			}
+//		}
 
 		//调用手机号验证码登录方法
 		$return  = (new UserService)->mobileCodeLogin($mobile,$logincode,$companyuser_id,$code);
