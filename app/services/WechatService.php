82f6f1fd05d8935df50af9896075c2439f38940e
1603633284@qq.com <1603633284@qq.com>
2020-06-02 18:19:48 +0800

    微信分享接口修改

--- app/services/WechatService.php
+++ app/services/WechatService.php
@@ -157,13 +157,13 @@ class WechatService extends BaseService
     }
 
     //获取分享所需参数
-    public function getSignPackage($appid="",$appsecret="")
+    public function getSignPackage($appid="",$appsecret="",$url="")
     {
         $AccessToken = $this->getAccessToken($appid,$appsecret);
         $jsapiTicket = $this->_newGetApiTicket($AccessToken);
         // 注意 URL 一定要动态获取，不能 hardcode.
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        //$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        //$url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         $timestamp = time();
         $nonceStr = $this->createNonceStr();
         // 这里参数的顺序要按照 key 值 ASCII 码升序排序
