598d3975a4cffeba347356652f308a197aebb0c8
Justin Chen <cxd032404@hotmail.com>
2020-06-22 16:52:30 +0800

    merge

--- app/services/WechatService.php
+++ app/services/WechatService.php
@@ -420,9 +420,39 @@ class WechatService extends BaseService
      */
     public function decryptData( $encryptedData, $iv, $wechat, $sessionKey )
     {
-        $decryptClass = new WXBizDataCrypt(['sessionKey'=>$sessionKey,'appId'=>$wechat['appid']]);
-        $data = $decryptClass->decryptData($encryptedData,$iv,$data);
-        return $data;
+        $appid = 'wx4f4bc4dec97d474b';
+        $sessionKey = 'tiihtNczf5v6AKRyjwEUhQ==';
+
+        $encryptedData="CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM
+                QmRzooG2xrDcvSnxIMXFufNstNGTyaGS
+                9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+
+                3hVbJSRgv+4lGOETKUQz6OYStslQ142d
+                NCuabNPGBzlooOmB231qMM85d2/fV6Ch
+                evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6
+                /1Xx1COxFvrc2d7UL/lmHInNlxuacJXw
+                u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn
+                /Hz7saL8xz+W//FRAUid1OksQaQx4CMs
+                8LOddcQhULW4ucetDf96JcR3g0gfRK4P
+                C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB
+                6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns
+                /8wR2SiRS7MNACwTyrGvt9ts8p12PKFd
+                lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV
+                oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG
+                20f0a04COwfneQAGGwd5oa+T8yO5hzuy
+                Db/XcxxmK01EpqOyuxINew==";
+
+        $iv = 'r7BXXKkLb8qrSNn05n0qiA==';
+        $decryptClass = new WXBizDataCrypt($appid,$sessionKey);
+
+        //$decryptClass = new WXBizDataCrypt($wechat['appid'],$sessionKey);
+        $errCode = $decryptClass->decryptData($encryptedData, $iv, $data );
+
+        if ($errCode == 0) {
+            print($data . "\n");
+        } else {
+            print($errCode . "\n");
+        }
+
     }
 
 }
