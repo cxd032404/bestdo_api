a8a7dead871a81d6d20b08a9b63b664b49cbcd78
shishuozheng <1697218933@qq.com>
2020-06-23 18:02:55 +0800

    去掉空数据

--- app/services/PageElementService.php
+++ app/services/PageElementService.php
@@ -804,6 +804,7 @@ class PageElementService extends BaseService
               $post_list = (new PostsService())->getPostsList($value,[],'list_id,views,title,source');
               if(!empty($post_list['data']))
               {
+                  continue;
                  $list_artical =  $post_list['data'][0];
                  $list_artical->title =  mb_substr($post_list['data'][0]->title,0,12,'utf-8');
                  $source = json_decode($list_artical->source,true);
