a86a99615c9149aa2e846a30c5f6a3fec115e63f
Justin Chen <cxd032404@hotmail.com>
2020-08-05 17:15:20 +0800

    merge

--- app/controllers/UploadController.php
+++ app/controllers/UploadController.php
@@ -5,9 +5,10 @@ class UploadController  extends BaseController
 {
 
     public function uploadAction(){
+        header('Content-Type: application/x-www-form-urlencoded');
         $oUpload = new UploadService();
         $upload = $oUpload->getUploadedFile([],[],0,0,['pic'=>1,'video'=>1,'txt'=>1]);
-        return $this->success($upload);
+        return $this->success(array_values($upload));
     }
 
 }
