b17b005bfade591697378b8a0a574ac6ec6fd30a
shishuozheng <1697218933@qq.com>
2020-09-18 16:43:55 +0800

    增加接口调用频次

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -534,7 +534,37 @@ class UserController extends BaseController
      */
 
     public function websiteMobileSaveAction(){
+        //获取ip
+        if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+            $ip = getenv('REMOTE_ADDR');
+        } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        $res =  preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+        //限制访问频次
+        $redis_key = 'ip'.$res;
+        $check = $this->redis->exists($redis_key);
+        if($check){
+            $this->redis->incr($redis_key);  //键值递增
+            $count = $this->redis->get($redis_key);
+            if($count > 5){
+                return $this->failure([],[],201);
+            }
+        }else{
+            $this->redis->incr($redis_key);
+            //限制时间为60秒
+            $this->redis->expire($redis_key,60);
+        }
+
+         $count = $this->redis->get($redis_key);
+
         $data = $this->request->getPost();
+
         $mobile = isset($data['mobile'])?substr(preg_replace('# #','',$data['mobile']),0,11):"";
         $return = (new UserService())->phoneNumberSave($mobile);
         if($return['result'] == 1)
